d11705e8b81c7eb3cefb8bba0c9494e6cdfc555c
Jose Eduardo Hernandez <86330577+JoseEduardoUAM@users.noreply.github.com>
2021-08-31 17:13:32 -0500

    Create programa.cpp

---
+++ programa.cpp
@@ -0,0 +1,10 @@
+#include "funciones.h"
+
+int main(){
+    int arr[4] = {};
+    std::cout << "Inicio el programa\n";
+    //descendenteExclusiva(arr);
+    //aditivaExclusiva(arr);
+    //descendenteAditivaExclusiva(arr);
+    ascendeteAditivaExclusiva(arr);
+}
