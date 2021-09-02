f83778b8ee56292db6e9af46bc67f8c3b252aac6
Jose Eduardo Hernandez <86330577+JoseEduardoUAM@users.noreply.github.com>
2021-09-02 16:43:06 -0500

    Update programa.cpp

--- programa.cpp
+++ programa.cpp
@@ -1,10 +1,8 @@
-#include "funciones.h"
+#include "lets.h"
 
 int main(){
-    int arr[4] = {};
-    std::cout << "Inicio el programa\n";
-    //descendenteExclusiva(arr);
-    //aditivaExclusiva(arr);
-    //descendenteAditivaExclusiva(arr);
-    ascendeteAditivaExclusiva(arr);
+    descendenteExclusiva();
+    //ascendenteExclusiva();
+    //descendenteAditivaExclusiva();
+    //ascendenteAditivaExclusiva();
 }
